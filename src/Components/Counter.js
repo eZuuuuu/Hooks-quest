a4497837c02f9c29eefb6c2d557d00031da2d022
enzo <enzo.seuve@laposte.net>
2020-05-09 16:27:33 +0200

    Modification du button, permet de modifer la valeur de départ dans l'input avec css

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
+import './Counter.css';
 
 function Counter() {
   // Déclare une nouvelle variable d'état, qu’on va appeler « count »
   // Déclare une fonction setCount qui permet de modifier la valeur de count
-  const [count, setCount, value ] = useState(0);
+  const [count, setCount ] = useState(0);
+  
+  const handleChangeName = e => setCount(e.target.value);
 
   return (
     <div>
-      <p>Le compteur est  à : {count} </p>
-      <input
+      <input className="counter"
           type="text"
           value={count}
-          onChange={e => value(e.target.value)}
+          onChange={handleChangeName}
         />
-      <button onClick={() => setCount(count + 1)}>
+      <button className="button" onClick={() => setCount(count + 1)}>
         +1
       </button>
-      <button onClick={() => setCount(count - 1)}>
+      <button className="button2" onClick={() => setCount(count - 1)}>
         -1
       </button>
     </div>
